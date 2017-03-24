4ade9acb91fb18035efd280b1ef700d070df5b2b
TheBestUser <a.kuzmichev@g.nsu.ru>
2017-03-24 15:12:17 +0700

    Pep8 Task E.

--- HomeWork1/E. Сортировка.py
+++ HomeWork1/E. Сортировка.py
@@ -1,5 +1,7 @@
 # E. Сортировка
-#import no_standard_sort
+# import no_standard_sort
+
+
 def quick_sort(arr):
     if arr:
         return quick_sort([x for x in arr if x < arr[0]]) + [x for x in arr if x == arr[0]] + quick_sort(
